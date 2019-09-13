113eb7b41d5bd77d1aef554597f81c755b79c0ed
Ahlam Fallatah <ahlamf@Ahlams-MacBook-Air.local>
2019-09-14 02:04:20 +0300

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,18 @@
+var cards;
+cards = ["queen", "queen", "king", "king"];
+var cardsInPlay;
+cardsInPlay = [];
+var cardOne;
+cards[0]="cardOne";
+cardsInPlay.push("cardOne");
+console.log("user flipped queen");
+var cardTwo;
+cards[2]="cardTwo";
+cardsInPlay.push("cardTwo");
+console.log("user flipped king");
+
+cardsInPlay.length;
+if (cardsInPlay.length===2);
+if (cardsInPlay.length === 2){};
+if (cardsInPlay[0] === cardsInPlay[1]) { alert("you found a match!");
+} else {alert("sorry, try again");}
