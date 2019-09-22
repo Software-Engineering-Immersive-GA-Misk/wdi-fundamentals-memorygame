9cb1e78a3f383c9fb270367415371426bb2f5bb4
Ahlam Fallatah <ahlamf@Ahlams-MacBook-Air.local>
2019-09-22 17:05:49 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,17 +2,28 @@ var cards;
 cards = ["queen", "queen", "king", "king"];
 var cardsInPlay;
 cardsInPlay = [];
-var cardOne;
-cards[0]="cardOne";
-cardsInPlay.push("cardOne");
-console.log("user flipped queen");
-var cardTwo;
-cards[2]="cardTwo";
-cardsInPlay.push("cardTwo");
-console.log("user flipped king");
+
+var checkForMatch = function(){
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+}
+
+var flipCard
+flipCard = function(cardId){
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push (cards[0]);
+if (cardsInPlay.length === 2) {checkForMatch();
+}
+}
 
 cardsInPlay.length;
 if (cardsInPlay.length===2);
 if (cardsInPlay.length === 2){};
 if (cardsInPlay[0] === cardsInPlay[1]) { alert("you found a match!");
 } else {alert("sorry, try again");}
+
+flipCard(0);
+flipCard(2);
