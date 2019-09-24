42fbca23f7212daaedf0a2d0971ff46d84c7af87
Ahlam Fallatah <ahlamf@Ahlams-MacBook-Air.local>
2019-09-24 18:29:09 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -32,21 +32,36 @@ if (cardsInPlay[0] === cardsInPlay[1]) {
 }
 }
 
-var flipCard
-flipCard = function(cardId){
+var flipCard;
+function flipCard(){
+var cardId = this.getAttribute('data-id');
 console.log("User flipped " + cards[cardId].rank);
-cardsInPlay.push(cards[cardId].rank);
+cardsInPlay.push(cards.rank);
 console.log("User flipped "+ cards[cardId].suit);
 console.log("User flipped " + cards[cardId].cardImage);
+this.setAttribute('src', cards[cardId].cardImage);
 if (cardsInPlay.length === 2) {checkForMatch();
 }
 if (cardsInPlay[0] === cardsInPlay[1]) {
   alert("You found a match!");
-} else {
+} 
+else {
   alert("Sorry, try again.");
 }
 }
 
- 
-flipCard(0);
-flipCard(2);
+var createBoard;
+function createBoard() 
+{
+   for (var i = 0; i < cards.length; i++) {
+   	var cardElement= document.createElement('img');
+   	cardElement.setAttribute('src', "images/back.png");
+   	cardElement.setAttribute('data-id', i);
+   	cardElement.addEventListener('click', flipCard);
+   	document.getElementById('game-board').appendChild(cardElement);
+   }
+}
+
+
+
+createBoard();
